c1da92431bfd165f9021c3f216746ed3295766c6
Giuseppe Abbate <giuseppeabbate94@gmail.com>
2024-04-11 15:56:55 +0200

    Initial commit

---
+++ lib/csproj_soup_generator.ex
@@ -0,0 +1,18 @@
+defmodule CsprojSoupGenerator do
+  @moduledoc """
+  Documentation for `CsprojSoupGenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsprojSoupGenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csproj_soup_generator_test.exs
@@ -0,0 +1,8 @@
+defmodule CsprojSoupGeneratorTest do
+  use ExUnit.Case
+  doctest CsprojSoupGenerator
+
+  test "greets the world" do
+    assert CsprojSoupGenerator.hello() == :world
+  end
+end
